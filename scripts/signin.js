4c4217692f1d87a3c72105e53bc7b9631530cd86
vanshika-2005 <158152785+vanshika-2005@users.noreply.github.com>
2024-10-22 14:56:28 +0530

    Update signin.js

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,5 +1,33 @@
 
-// Get the back-to-top button
+document.addEventListener('DOMContentLoaded', function() {
+    const storedEmail = localStorage.getItem('email');
+    const storedPassword = localStorage.getItem('password');
+
+    if (storedEmail) {
+        document.getElementById('email').value = storedEmail;
+    }
+    if (storedPassword) {
+        document.getElementById('password').value = storedPassword;
+    }
+});
+
+document.getElementById('signinForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('errorMessage');
+
+    if (email === localStorage.getItem('email') && password === localStorage.getItem('password')) {
+        alert('Sign In Successful!');
+        
+    } else {
+       
+        errorMessage.style.display = 'block';
+    }
+});
+
+
+
 const backToTopButton = document.getElementById('back-to-top');
 
 
